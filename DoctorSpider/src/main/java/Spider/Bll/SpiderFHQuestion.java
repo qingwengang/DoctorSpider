995e346fc0a0c3cff3d6dc30186b6ea029e27af2
qingwengang <qingwengang@qq.com>
2016-06-08 20:32:36 +0800

    fh

--- DoctorSpider/src/main/java/Spider/Bll/SpiderFHQuestion.java
+++ DoctorSpider/src/main/java/Spider/Bll/SpiderFHQuestion.java
@@ -1,8 +1,18 @@
 package Spider.Bll;
 
+import Util.JsoupUtil;
+import org.jsoup.nodes.Document;
+
 /**
  * Created by Administrator on 2016/6/8.
  */
 public class SpiderFHQuestion {
-
+    public static void main(String[] args){
+        Spider(102785561);
+    }
+    private static void Spider(long id){
+        String url="http://iask.fh21.com.cn/question/"+id+".html";
+        Document doc = JsoupUtil.GetDocument(url);
+        System.out.print(doc.select(".iask_navigator").get(0).text());
+    }
 }
