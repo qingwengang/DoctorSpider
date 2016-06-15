3d33f05628e5f753a646d6cdf962efd99b86c15b
qingwengang <qingwengang@qq.com>
2016-06-15 18:05:33 +0800

    好大夫

--- DoctorSpider/项目说明文件/doctor.sql
+++ DoctorSpider/项目说明文件/doctor.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : local
-Source Server Version : 50096
-Source Host           : 127.0.0.1:3306
+Source Server Version : 50162
+Source Host           : localhost:3306
 Source Database       : doctor
 
 Target Server Type    : MYSQL
-Target Server Version : 50096
+Target Server Version : 50162
 File Encoding         : 65001
 
-Date: 2016-06-11 21:49:57
+Date: 2016-06-15 18:04:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,12 +20,12 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `answer`;
 CREATE TABLE `answer` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `DoctorId` varchar(50) default NULL,
-  `QuestionId` bigint(20) default NULL,
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `DoctorId` varchar(50) DEFAULT NULL,
+  `QuestionId` bigint(20) DEFAULT NULL,
   `Content` text,
-  `AnswerTime` datetime default NULL,
-  PRIMARY KEY  (`Id`)
+  `AnswerTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -33,56 +33,42 @@ CREATE TABLE `answer` (
 -- ----------------------------
 DROP TABLE IF EXISTS `askquestion`;
 CREATE TABLE `askquestion` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Type` varchar(30) default NULL,
-  `OutId` varchar(30) default NULL,
-  `AuthorTime` datetime default NULL,
-  `SpiderFlag` int(10) default '0',
-  PRIMARY KEY  (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1187405 DEFAULT CHARSET=utf8;
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Type` varchar(30) DEFAULT NULL,
+  `OutId` varchar(30) DEFAULT NULL,
+  `AuthorTime` datetime DEFAULT NULL,
+  `SpiderFlag` int(10) DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=150779 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for doctorinfo
 -- ----------------------------
 DROP TABLE IF EXISTS `doctorinfo`;
 CREATE TABLE `doctorinfo` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `DocId` varchar(50) default NULL,
-  `Name` varchar(50) default NULL,
-  `Title` varchar(200) default NULL,
-  `Expertise` varchar(100) default NULL,
-  `LastSpiderTime` datetime default NULL,
-  `Type` int(11) default '0',
-  PRIMARY KEY  (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=162104 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for fhquestion
--- ----------------------------
-DROP TABLE IF EXISTS `fhquestion`;
-CREATE TABLE `fhquestion` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `OutId` varchar(20) default NULL,
-  `MuluId` bigint(20) default NULL,
-  `MuluName` varchar(20) default NULL,
-  `SpiderFlag` int(11) default NULL,
-  `WriteFileFlag` int(11) default '0',
-  PRIMARY KEY  (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2000001 DEFAULT CHARSET=utf8;
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `DocId` varchar(50) DEFAULT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Title` varchar(200) DEFAULT NULL,
+  `Expertise` varchar(100) DEFAULT NULL,
+  `LastSpiderTime` datetime DEFAULT NULL,
+  `Type` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6845 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for hdfdoctorinfo
 -- ----------------------------
 DROP TABLE IF EXISTS `hdfdoctorinfo`;
 CREATE TABLE `hdfdoctorinfo` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Name` varchar(20) default NULL,
-  `HospitalName` varchar(50) default NULL,
-  `Major` varchar(20) default NULL,
-  `OutId` varchar(50) default NULL,
-  `Title` varchar(30) default NULL,
-  `GetContentFlag` int(11) default '0',
-  PRIMARY KEY  (`Id`)
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) DEFAULT NULL,
+  `HospitalName` varchar(50) DEFAULT NULL,
+  `Major` varchar(20) DEFAULT NULL,
+  `OutId` varchar(50) DEFAULT NULL,
+  `Title` varchar(30) DEFAULT NULL,
+  `GetContentFlag` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -90,77 +76,112 @@ CREATE TABLE `hdfdoctorinfo` (
 -- ----------------------------
 DROP TABLE IF EXISTS `hdfmulu`;
 CREATE TABLE `hdfmulu` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Name` varchar(50) default NULL,
-  `Url` varchar(200) default NULL,
-  `SpiderFlag` int(11) default NULL,
-  `Level` int(11) default NULL,
-  `TagName` varchar(50) default NULL,
-  PRIMARY KEY  (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1479 DEFAULT CHARSET=utf8;
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) DEFAULT NULL,
+  `Url` varchar(200) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT NULL,
+  `Level` int(11) DEFAULT NULL,
+  `TagName` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2957 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for hdfpage
 -- ----------------------------
 DROP TABLE IF EXISTS `hdfpage`;
 CREATE TABLE `hdfpage` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Riqi` varchar(20) default NULL,
-  `Url` varchar(255) default NULL,
-  `SpiderFlag` int(11) default '0',
-  PRIMARY KEY  (`Id`)
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Riqi` varchar(20) DEFAULT NULL,
+  `Url` varchar(255) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3055 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for hdfpagedetail
+-- ----------------------------
+DROP TABLE IF EXISTS `hdfpagedetail`;
+CREATE TABLE `hdfpagedetail` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Url` varchar(255) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for jibing
 -- ----------------------------
 DROP TABLE IF EXISTS `jibing`;
 CREATE TABLE `jibing` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Name` varchar(50) default NULL,
-  `Suoxie` varchar(50) default NULL,
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) DEFAULT NULL,
+  `Suoxie` varchar(50) DEFAULT NULL,
   `Jianjie` text,
   `Zhengzhuang` text,
   `FBYY` text,
-  `GetContentFlag` int(11) default '0',
-  `Url` varchar(255) default NULL,
-  PRIMARY KEY  (`Id`)
+  `GetContentFlag` int(11) DEFAULT '0',
+  `Url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8091 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for jjquestion
+-- ----------------------------
+DROP TABLE IF EXISTS `jjquestion`;
+CREATE TABLE `jjquestion` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TypeName` varchar(50) DEFAULT NULL,
+  `OutId` bigint(20) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123664 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for jkquestion
+-- ----------------------------
+DROP TABLE IF EXISTS `jkquestion`;
+CREATE TABLE `jkquestion` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `TypeName` varchar(50) DEFAULT NULL,
+  `OutId` bigint(20) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=123664 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for mulu
 -- ----------------------------
 DROP TABLE IF EXISTS `mulu`;
 CREATE TABLE `mulu` (
-  `Id` bigint(11) NOT NULL auto_increment,
-  `Name` varchar(20) default NULL,
-  `Url` varchar(255) default NULL,
-  `Source` varchar(20) default NULL,
-  `SpiderFlag` int(11) default NULL,
-  `Level` int(11) default NULL,
-  `ParentId` bigint(11) default NULL,
-  `Type` varchar(20) default NULL,
-  `AskId` int(11) default NULL,
-  PRIMARY KEY  (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=897 DEFAULT CHARSET=utf8;
+  `Id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) DEFAULT NULL,
+  `Url` varchar(255) DEFAULT NULL,
+  `Source` varchar(20) DEFAULT NULL,
+  `SpiderFlag` int(11) DEFAULT NULL,
+  `Level` int(11) DEFAULT NULL,
+  `ParentId` bigint(11) DEFAULT NULL,
+  `Type` varchar(20) DEFAULT NULL,
+  `AskId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1984 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for question
 -- ----------------------------
 DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
-  `Id` bigint(20) NOT NULL auto_increment,
-  `Title` varchar(2000) default NULL,
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(2000) DEFAULT NULL,
   `Content` text,
-  `Type` varchar(50) default NULL,
-  `GetContentFlag` int(11) default '0',
-  `AskId` bigint(20) default NULL,
-  `Description` varchar(255) default NULL,
-  `NeedHelp` varchar(255) default NULL,
-  `AuthorId` varchar(50) default NULL,
-  `AuthorTime` datetime default NULL,
-  `LastSpiderTime` datetime default NULL,
-  `AuthorSex` varchar(20) default NULL,
-  `Laiyuan` varchar(20) default NULL,
-  PRIMARY KEY  (`Id`)
+  `Type` varchar(50) DEFAULT NULL,
+  `GetContentFlag` int(11) DEFAULT '0',
+  `AskId` bigint(20) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `NeedHelp` varchar(255) DEFAULT NULL,
+  `AuthorId` varchar(50) DEFAULT NULL,
+  `AuthorTime` datetime DEFAULT NULL,
+  `LastSpiderTime` datetime DEFAULT NULL,
+  `AuthorSex` varchar(20) DEFAULT NULL,
+  `Laiyuan` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
